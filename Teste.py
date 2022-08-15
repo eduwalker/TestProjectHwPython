8cf7f0462c028ab3c4f730bde133dfbfb3e54979
eduwalker <eduardo.venula@gmail.com>
2022-08-14 22:53:11 -0400

    TesteCommit Phyton

---
+++ Teste.py
@@ -0,0 +1,13 @@
+#coding: utf-8
+
+
+
+initialize_cod = "HELOW WORLD !!!"
+meu_nome = "Eduardo Oliveira"
+meu_nick = "Walker"
+
+
+
+print(initialize_cod)
+print("Nome: %s" % (meu_nome))
+print("Nick in Game: %s" % (meu_nick))
